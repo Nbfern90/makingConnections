1e006b4b9ffdc2b0a3a9b433c0fc9306a3aa0f8d
Nick Fernandez <nbfern90@gmail.com>
2021-07-25 18:18:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function changeText(){
+document.querySelector("#janeName").innerHTML ="Jane Dough";
+}
+
+function hide(){
+  document.querySelector("#todd").remove();
+}
+
+function hideToo(){
+  document.querySelector("#phil").remove();
+}
